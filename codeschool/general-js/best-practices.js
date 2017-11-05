9d45eb78248fb94f3484f10f0614fce707e83712
Dean <dean.wu55@gmail.com>
2017-11-05 15:27:54 -0500

    almost done codeschool js best practices

---
+++ codeschool/general-js/best-practices.js
@@ -0,0 +1,89 @@
+// ternary
+// can nest...
+console.log('Current weapon: ' + (isArthur && isKing ? 'Excalibur' : 'Longsword'));
+
+// logical or
+// if all elements falsy, last value assigned 
+this.foo = this.foo || 'bar'; 
+
+// logical and...
+
+// switch block
+// fall-through can be useful
+
+// loop optimization
+// cache values
+for (var i = 0; i < treasureChest.necklaces.length; i++) {
+	console.log(treasureChest.necklaces[i]);
+}
+
+var list = treasureChest.necklaces;
+for (var i = 0, x = list.length; i < x; i++) {
+	console.log(list[i]);
+}
+
+// script execution
+// relocate work-intensive scripts to the bottom of the page
+// use HTML5 aysnc attribute
+
+// short performance tips
+
+// add common methods to obj prototype
+// necessary if using classes?
+
+// use document fragments to append all DOM elements at once
+// instead of appending a single element at a time
+
+var fragment = document.createDocumentFragment(),
+	elem;
+for (var i = 0, x = foo.length; i < x; i++) {
+	elem = document.createElement('li');
+	elem.appendChild(document.createTextNode(foo[i]));
+	fragment.appendChild(elem);
+}
+document.getElementById('foo').appendChild(fragment);
+
+// concatentating strings
+// += generall good
+// .join for array of strings (not +=)
+
+// measuring performance
+// console.time and console.timeEnd
+
+var startTime = +new Date(); // + unary operator indicates math op and tries to convert element into number
+var endTime = +new Date();
+var elapsedTime = endTime - startTime;
+
+// with a speed test class
+// http://campus.codeschool.com/courses/javascript-best-practices/level/2/section/5/video/1
+
+// careful comparisons
+// use ===
+// use instanceof for classes
+
+// error handling
+// try catch blocks
+// throw new Error
+
+// stuff that sucks
+// with() sucks; use variables to cache instead
+// eval() sucks
+// not using curly braces sucks
+
+// number stuff
+// parseInt(number, 10); 
+// check for number presence first
+// to check for number --> typeof data === 'number' && !isNaN(data)***
+// typeof NaN is a number and thus second part is necessary 
+
+// namespacing 
+
+
+
+
+
+
+
+
+
+
