adf9cc37c38440dd945ded1442820fafbd68942a
Dean <dean.wu55@gmail.com>
2017-11-09 15:01:59 -0500

    starting 2nd codeschool SQL course

--- codeschool/SQL/try-SQL/sql.sql
+++ codeschool/SQL/try-SQL/sql.sql
@@ -27,3 +27,20 @@ WHERE item = 'popcorn' OR item = 'candy';
 DELETE FROM movies WHERE genre = 'comedy';
 
 DELETE FROM movies WHERE duration > 120 OR title = 'inception';
+
+CREATE TABLE advertisements
+(
+	id int,
+	name varchar(10),
+	category varchar(10),
+	cost int
+);
+
+INSERT INTO advertisements (id, name, cost)
+VALUES (1, 'commercial', 150);
+
+UPDATE advertisements
+SET category = 'television'
+WHERE name = 'commercial';
+
+DROP TABLE advertisements;
